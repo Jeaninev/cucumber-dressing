22431f5a9786705b25199a3dd63271651d3e1aa3
Jeanine van Velzen <jeanine@centurus.it>
2023-03-13 15:11:38 +0100

    Added javadoc

--- src/main/java/com/vijane/cucumber/dressing/DataElementTransformer.java
+++ src/main/java/com/vijane/cucumber/dressing/DataElementTransformer.java
@@ -22,6 +22,56 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.cucumber.datatable.DataTable;
+
+/**
+ * {@code @DataElementTransformer} is used to mark that annotated field is part of a <em>DataTable</em> element.
+ * <p>
+ * {@code @DataElementTransformer} fields should be {@code private} at class level.
+ * <p>
+ * <h2>Usage</h2>
+ * <p>
+ * The simple {@code DataTable} can be converted using the field declarations below.
+ * <p>
+ * <pre>
+ *    Given the following dataTable
+ *    | amount | listTypes | someName |
+ *    | 1.23   | aa,bb,cc  | VALUE1   |
+ *    | 4.56   | dd,ee,ff  | VALUE2   |
+ * </pre>
+ * <p>
+ * By default annotated fields simply converts string values from columns in {@code DataTable} to their type. This works
+ * for most types that have a constructor accepting a single {@code String} parameter:
+ * <p>
+ * <pre>
+ *    &#064;DataElementTransformer
+ *    private BigDecimal amount;
+ * </pre>
+ * <p>
+ * Lists need a little help. This can be achieved by passing a few simple parameters to the annotation, like:
+ * <p>
+ * <pre> 
+ *    &#064;DataElementTransformer( converter = Arrays.class, method = "asList(java.lang.Object[])" )
+ *    private List<String> listTypes; 
+ * </pre>
+ * <p>
+ * <em>Note: Be careful to use the proper method signature</em> By default all fields in the class are mandatory. Set
+ * the mandatory parameter to false to make a field optional, or provide a default value for the field:
+ * <p>
+ * <pre>
+ *    &#064;DataElementTransformer( converter = Arrays.class, method = "asList(java.lang.Object[])", mandatory = false, defaultValue = "no, nothing was set" )
+ *    private List<String> listTypesWithDefault;
+ * </pre>
+ * <p>
+ * Enum's can be implemented as below:
+ * <p>
+ * <pre>
+ *    &#064;DataElementTransformer(value = "someName", converter = EnumType.class, method = "valueOf(java.lang.String)")
+ *    private EnumType enumType;
+ * </pre>
+ *
+ * @see DataTable
+ */
 @Retention( RetentionPolicy.RUNTIME )
 @Target( { TYPE, FIELD } )
 public @interface DataElementTransformer
